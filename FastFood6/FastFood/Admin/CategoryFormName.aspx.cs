1705af5313f3bdade5bf6303bb664201ab46b7fa
unknown <poncefernandoandres@gmail.com>
2024-09-19 19:37:28 -0300

    add

---
+++ FastFood6/FastFood/Admin/CategoryFormName.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLogic;
+using Dominio;
+
+namespace FastFood.Admin
+{
+    public partial class CategoryFormName : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnNext_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                BusinessCategories business = new BusinessCategories();
+                CategoryD addCategory = new CategoryD();
+
+                addCategory.Name = txtName.Text;
+
+                business.AddWithSP(addCategory);
+
+                string nameRef = txtName.Text;
+                Response.Redirect("CategoryForm.aspx?Name=" + nameRef, false);
+
+
+            }
+            catch (Exception ex)
+            {
+                Session.Add("Error", ex.ToString());
+                Response.Redirect("Error.aspx", false);
+            }
+        }
+    }
+}
