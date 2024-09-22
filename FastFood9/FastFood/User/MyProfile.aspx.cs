e34e683a58d92f158c34a1f200e25e7503e771f6
unknown <poncefernandoandres@gmail.com>
2024-09-22 15:18:09 -0300

    add

---
+++ FastFood9/FastFood/User/MyProfile.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FastFood.User
+{
+    public partial class MyProfile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            txtUserName.Enabled = false;
+            txtEmail.Enabled = false;
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+
+            Response.Redirect("About.aspx", false);
+        }
+    }
+}
---
+++ FastFood9/FastFood/User/User.Master.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FastFood.User
+{
+    public partial class User : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Url.AbsoluteUri.ToString().Contains("Default.aspx"))
+            {
+                //Cargamos la pagina de control, para usar el sliderUser.
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl.ascx");
+                pnlSliderUC.Controls.Add(sliderUserControl);
+                form1.Attributes.Remove("class");
+            }
+            else
+            {
+                form1.Attributes.Add("class", "sub_page");
+
+            }
+
+        }
+    }
+}
