73b28eb4cd3d6baa56151b0d6363997787e79982
unknown <poncefernandoandres@gmail.com>
2024-09-30 20:04:50 -0300

    add

---
+++ FastFood18Seguir/FastFood/User/Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLogic;
+using Dominio;
+
+namespace FastFood.User
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!IsPostBack)
+                {
+
+                    BusinessProducts businessProduct = new BusinessProducts();
+                    BusinessCategories businessCategory = new BusinessCategories();
+
+                    List<ProductD> listProduct = businessProduct.ListProductsActive();
+                    List<CategoryD> listCategory = businessCategory.ListCategoryActive();
+
+                    rCategory.DataSource = listCategory;
+                    rCategory.DataBind();
+                    //rProducts.DataSource = listProduct;
+                    //rProducts.DataBind();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Session.Add("Error", ex.ToString());
+                Response.Redirect("Error.aspx", false);
+            }
+
+        }
+    }
+}
