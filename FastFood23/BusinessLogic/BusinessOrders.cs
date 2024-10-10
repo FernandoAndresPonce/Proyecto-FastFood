a9e88525ae9e7404f511d7ba207e40a5cbb1367b
unknown <poncefernandoandres@gmail.com>
2024-10-10 14:24:13 -0300

    add

---
+++ FastFood23/BusinessLogic/BusinessOrders.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using Dominio;
+
+namespace BusinessLogic
+{
+    public class BusinessOrders
+    {
+
+        private Connection data;
+
+        public BusinessOrders()
+        {
+            this.data = new Connection();
+        }
+
+        public void AddWithSP(OrdersD order)
+        {
+
+            try
+            {
+                data.clearParameter();
+                data.storedProcedureConsult("storedAddOrder");
+
+                data.SetParameter("@PaymentId", order.Payment.PaymentId);
+                data.SetParameter("@ProductId", order.Product.ProductId);
+                data.SetParameter("@OrderNo", order.OrderNo);               
+                data.SetParameter("@Quantity", order.Quantity);
+                data.SetParameter("@UserId", order.User.UserId);
+                data.SetParameter("@Status", order.Status);
+                data.SetParameter("@OrderDate", order.OrderDate);
+
+                data.executeAction();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                data.closeConection();
+            }
+        }
+    }
+}
